c4708bcd2219e7fac60562b13793f6fc81fedcb8
meral-memeti <meralmemetii@gmail.com>
2022-05-30 16:49:16 +0200

    structuring

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styles from  "./styles.module.scss";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {Home} from "./components/Home";
+import { TipContext } from "./contextProvider/TipContext";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+    <BrowserRouter>
+        <TipContext>
+        <Routes>
+            <Route path="/*" element={<Home />} />
+        </Routes>
+        </TipContext>
+    </BrowserRouter>
+    );
 }
 
 export default App;
